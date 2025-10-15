3370a0438d057c51b855f589aa95cae2d0d7ac0e
Ying <3153631954@qq.com>
2025-10-15 17:08:55 +0800

    Fix

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,11 +4,11 @@ public class Calc {
 
     public int add(int x, int y)
     {
-        return x+y;
+        return x + y;
     }
 
     public int subtract(int x, int y)
     {
-        return x*y;
+        return x - y;
     }
 }
